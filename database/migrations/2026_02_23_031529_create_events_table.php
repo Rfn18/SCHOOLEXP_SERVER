93a6dcf693baa6f5751f388615962f4109f02e65
Rfn18 <rinofaster89@gmail.com>
2026-02-23 10:34:51 +0700

    init: Set Up Laravel for backend

---
+++ database/migrations/2026_02_23_031529_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('location');
+            $table->string('poster');
+            $table->date('date');
+            $table->string('status')->default('upcoming');
+            $table->boolean('is_all_day')->default(false);
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+            $table->foreignId('event_category_id')
+                ->constrained('event_categories')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
